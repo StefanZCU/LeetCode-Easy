ae642ad32353210d05e16d6db66be02e698f0760
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2022-11-27 11:25:07 +0200

    Added LeetCode-Easy Folder

---
+++ LeetCode-Easy/01. Two Sum/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        int[] foundNums = new int[2];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target && i != j)
+                {
+                    foundNums[0] = i;
+                    foundNums[1] = j;
+                    return foundNums;
+                }
+            }
+        }
+
+        return foundNums;
+
+    }
+}
---
+++ LeetCode-Easy/09. Palindrome Number/Program.cs
@@ -0,0 +1,52 @@
+﻿public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        int counter = 0;
+        string num = x.ToString();
+
+        if (num.Length % 2 == 0)
+        {
+            int half = num.Length / 2;
+
+            for (int i = 0; i < half; i++)
+            {
+                if (num[i] == num[num.Length - i - 1])
+                {
+                    counter++;
+                }
+            }
+
+            if (counter == half)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        else
+        {
+            int half = num.Length / 2;
+
+            for (int i = 0; i < half; i++)
+            {
+                if (num[i] == num[num.Length - i - 1])
+                {
+                    counter++;
+                }
+            }
+
+            if (counter == half)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
