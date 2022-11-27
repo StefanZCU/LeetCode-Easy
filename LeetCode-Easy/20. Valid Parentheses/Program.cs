209111ec02f9bd7e7d66b43651388dadf25dbf0d
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2022-11-27 11:52:37 +0200

    Added 20. Valid Parentheses

---
+++ LeetCode-Easy/20. Valid Parentheses/Program.cs
@@ -0,0 +1,48 @@
+﻿public class Solution
+{
+    public bool IsValid(string s)
+    {
+        char[] array = s.ToCharArray();
+        Stack<char> stack = new Stack<char>();
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == '(' || array[i] == '[' || array[i] == '{')
+            {
+                stack.Push(array[i]);
+                continue;
+            }
+
+            if (stack.Count != 0)
+            {
+                if (array[i] == ')' && stack.Peek() == '(')
+                {
+                    stack.Pop();
+                }
+                else if (array[i] == ']' && stack.Peek() == '[')
+                {
+                    stack.Pop();
+                }
+                else if (array[i] == '}' && stack.Peek() == '{')
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        if (stack.Count != 0)
+        {
+            return false;
+        }
+        
+        return true;
+    }
+}
