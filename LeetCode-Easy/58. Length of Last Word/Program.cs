d763c70a4520ed3539526e1ba1275db69a8f374b
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-12 13:34:42 +0200

    Added 58. Length of Last Word

---
+++ LeetCode-Easy/58. Length of Last Word/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        s = s.TrimEnd();
+        int length = 0;
+
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            if (s[i] == ' ')
+            {
+                return length;
+            }
+
+            length++;
+        }
+
+        return length;
+    }
+}
