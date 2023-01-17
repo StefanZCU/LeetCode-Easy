34af1c8aece42568350c79790b6ad3decafb92be
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-17 15:13:06 +0200

    temp 566.

--- LeetCode-Easy/566. Reshape the Matrix/Program.cs
+++ LeetCode-Easy/566. Reshape the Matrix/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution
+{
+    public int[][] MatrixReshape(int[][] mat, int r, int c)
+    {
+        
+    }
+}
