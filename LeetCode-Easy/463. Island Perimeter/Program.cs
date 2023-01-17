08322cbf66ab18946e416d1c8fe86cbc310f3a43
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-17 14:01:28 +0200

    Added 463. Island Perimeter

---
+++ LeetCode-Easy/463. Island Perimeter/Program.cs
@@ -0,0 +1,49 @@
+﻿
+public class Solution
+{
+    public int IslandPerimeter(int[][] grid)
+    {
+        int parameterCount = 0;
+
+        for (int i = 0; i < grid.GetLength(0); i++)
+        {
+            for (int j = 0; j < grid[i].Length; j++)
+            {
+                if (grid[i][j] == 1)
+                {
+                    int[,] direction =
+                    {
+                        {i - 1, j},
+                        {i + 1, j},
+                        {i, j + 1},
+                        {i, j - 1}
+                    };
+
+                    for (int k = 0; k < direction.GetLength(0); k++)
+                    {
+                        if (IndexChecker(grid, direction[k, 0], direction[k, 1]))
+                        {
+                            if (grid[direction[k, 0]][direction[k, 1]] == 0)
+                            {
+                                parameterCount++;
+                            }
+                        }
+                        else
+                        {
+                            parameterCount++;
+                        }
+
+                    }
+                }
+            }
+        }
+
+        return parameterCount;
+    }
+
+    public bool IndexChecker(int[][] grid, int row, int col)
+    {
+        return row >= 0 && row < grid.GetLength(0) &&
+               col >= 0 && col < grid[row].Length;
+    }
+}
