5933418689969643c230b6ed592b999554f227a1
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-01-19 15:06:33 +0200

    Added 1556. Thousand Separator

---
+++ LeetCode-Easy/1556. Thousand Seperator/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Globalization;
+
+public class Solution
+{
+    public string ThousandSeparator(int n)
+    {
+        var format = new NumberFormatInfo { NumberGroupSeparator = "." };
+        return n.ToString("N0", format);
+    }
+}
