e0fb67b9c3d6408dfd15d027d2c160f0ec2f9bfc
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-02-27 12:13:14 +0200

    Added 1480. Running Sum of 1d Array

--- LeetCode-Easy/1480. Running Sum of 1d Array/Program.cs
+++ LeetCode-Easy/1480. Running Sum of 1d Array/Program.cs
@@ -2,6 +2,11 @@
 {
     public int[] RunningSum(int[] nums)
     {
+        for (int i = 1; i < nums.Length; i++)
+        {
+            nums[i] += nums[i - 1];
+        }
 
+        return nums;
     }
 }
