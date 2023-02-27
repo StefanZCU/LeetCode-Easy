bdd97c25bd550d7ae46c673498ecc6293f1ffb1b
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-02-27 12:20:31 +0200

    Added 0724. Find Pivot Index

---
+++ LeetCode-Easy/0724. Find Pivot Index/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int PivotIndex(int[] nums)
+    {
+        int sum = nums.Sum();
+        int leftSum = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (leftSum == sum - leftSum - nums[i])
+            {
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+}
