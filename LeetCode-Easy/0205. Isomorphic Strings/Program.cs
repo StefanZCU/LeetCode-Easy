5c74b7f52e7bf0d15d07d441b5454672900f34fe
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-02-27 12:28:33 +0200

    Added 0205. Isomorphic Strings

---
+++ LeetCode-Easy/0205. Isomorphic Strings/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Solution
+{
+    public bool IsIsomorphic(string s, string t)
+    {
+        if (s.Length == 0)
+        {
+            return true;
+        }
+
+        var listS = new List<char>();
+        var listT = new List<char>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            var indexS = listS.IndexOf(s[i]);
+            var indexT = listT.IndexOf(t[i]);
+
+            if (indexS != indexT)
+            {
+                return false;
+            }
+            if (indexS < 0)
+            {
+                listS.Add(s[i]);
+            }
+            if (indexT < 0)
+            {
+                listT.Add(t[i]);
+            }
+        }
+
+        return true;
+    }
+}
