8a17249cc70995fbf0fb28b6cce11a8b41ac0ab9
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-02-28 13:26:58 +0200

    Update Program.cs

--- LeetCode-Easy/0067. Add Binary/Program.cs
+++ LeetCode-Easy/0067. Add Binary/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution
+{
+    public string AddBinary(string a, string b)
+    {
+        var sum = new List<int>();
+        for (int i = a.Length - 1, j = b.Length - 1, carry = 0; i >= 0 || j >= 0 || carry > 0;)
+        {
+            var firstDigit = i >= 0 ? a[i--] - '0' : 0;
+            var secondDigit = j >= 0 ? b[j--] - '0' : 0;
+            var sumDigit = firstDigit + secondDigit + carry;
+            carry = sumDigit / 2;
+            sum.Add(sumDigit % 2);
+        }
+        sum.Reverse();
+        return String.Concat(sum);
+    }
+}
