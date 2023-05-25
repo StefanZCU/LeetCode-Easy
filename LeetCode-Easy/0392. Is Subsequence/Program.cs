517b8cfb6cf69b61b13c44cf3859e3f5448a7ea9
StefanZCU <114801161+StefanZCU@users.noreply.github.com>
2023-05-25 12:54:48 +0300

    Added temp 0392.

---
+++ LeetCode-Easy/0392. Is Subsequence/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+public class Solution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        int tIndex = 0;
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            for (int j = tIndex; j < t.Length; j++)
+            {
+                if (i + 1 > s.Length)
+                {
+                    if (s[i + 1] == t[j])
+                    {
+                        return false;
+                    }
+
+                    if (s[i] != t[j]) continue;
+                    sb.Append(s[i]);
+                    tIndex++;
+                    break;
+                }
+
+                if (s[i] != t[j]) continue;
+                sb.Append(s[i]);
+                tIndex++;
+                break;
+            }
+        }
+
+        return sb.ToString() == s;
+    }
+}
